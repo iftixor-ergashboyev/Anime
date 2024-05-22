5f5611edaaf81f8eac5cee4d73c8c7ae599833af
iftixorFSD <etgashboeviftixor@gmail.com>
2024-05-22 16:57:57 +0500

    anime

--- lib/presentation/viewmodel/bm_provider.dart
+++ lib/presentation/viewmodel/bm_provider.dart
@@ -1,9 +1,11 @@
+import 'package:anime/data/repository/local_repository_impl.dart';
+import 'package:anime/domain/model/anime_db.dart';
 import 'package:flutter/cupertino.dart';
-import '../../data/repository/local_repository_impl.dart';
-import '../../domain/model/anime_db.dart';
 
 class BmProvider extends ChangeNotifier {
   final _local = LocalRepositoryImpl();
+  // final List<AnimeDb> animeList = [];
+
   Stream<List<AnimeDb>>? fetchLocalAnimeList() => _local.getFavoriteAnimeList();
 
   void delete(AnimeDb anime) async {
